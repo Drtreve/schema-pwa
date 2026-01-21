45d05e9991a6fa99ad565be76debbbdfa0404d6b
Drtreve <106742783+Drtreve@users.noreply.github.com>
2026-01-21 13:15:36 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('schema-v5').then((cache) =>
-      cache.addAll(['./', './index.html', './manifest.json'])
+    caches.open('schema-v6').then((cache) =>
+      cache.addAll(['./', './index.html', './manifest.json']) 
+      cache.addAll(['./', './index.html', './manifest.json', './override.css'])                            
     )
   );
 });
